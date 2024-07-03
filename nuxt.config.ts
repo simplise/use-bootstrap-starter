10e606192faf14508b16679810c527644158c2c3
KoujiSano <sano@simplise.jp>
2024-07-03 18:03:20 +0900

    npm install usebootsrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-03"
+  compatibilityDate: "2024-07-03",
+  modules: ["usebootstrap"],
+  css: [
+    "bootstrap/scss/bootstrap.scss"
+  ]
 })
