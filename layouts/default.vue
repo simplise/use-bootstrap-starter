133b58dbece742bc62802b43b215090dfa1fd2eb
KoujiSano <sano@simplise.jp>
2024-09-02 00:33:14 +0900

    3.5.44

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,48 +1,48 @@
 <template>
-  <ShareHeader />
-  <Container type="xl" padding="y-md-2">
-    <GridTemplate areas="s e c" areas-lg="sce" rows="auto 1fr auto" rows-lg="1fr" columns="100%"
-      columns-lg="1fr 640px 160px" columns-xl="1fr 760px 1fr" columns-xxl="1fr 880px 1fr">
-      <GridArea area="s" margin="e-md-4 s-md-3 s-xl-0" overflow="y-auto x-hidden" sticky="lg-top" z="1"
-        class="area-s un-top-5rem">
-        <ViewState v-slot="navigation" src="route://meta" path="navigation">
-          <Sidebar id="sidebar" :src="`app-config://usebootstrap/navigations/${navigation.data || 'default'}`" />
-        </ViewState>
-      </GridArea>
-      <GridArea area="c">
-        <Container type="8" margin="x-auto">
-          <Intro />
-          <Toc selector="#content" display="block lg-none" class="toc">
-            <Localization>
-              <template #en>
-                On this page
-              </template>
-              <template #ja>
-                項目
-              </template>
-            </Localization>
-          </Toc>
-          <b-div id="content" padding="s-lg-2">
-            <slot />
-          </b-div>
-        </Container>
-      </GridArea>
-      <GridArea area="e" overflow="y-auto x-hidden" sticky="lg-top" z="0" class="area-e un-h-min un-top-5rem">
-        <Toc selector="#content" display="none lg-block" class="toc">
-          <Localization>
-            <template #en>
-              On this page
-            </template>
-            <template #ja>
-              項目
-            </template>
-          </Localization>
-        </Toc>
-      </GridArea>
-    </GridTemplate>
-  </Container>
-  <ShareFooter />
-  <BackToTop />
+ <PageLayout type="TwoColumnsBetween">
+  <template #header>
+   <ShareHeader />
+  </template>
+  <template #start>
+   <ViewState
+    v-slot="navigation"
+    src="route://meta"
+    path="navigation"
+   >
+    <Sidebar
+     id="sidebar"
+     :src="`app-config://usebootstrap/navigations/${navigation.data || 'default'}`"
+    />
+   </ViewState>
+  </template>
+  <template #end>
+   <Toc
+    selector="#content"
+    display="none lg-block print-none"
+    color="teal-700"
+   >
+    <Localization>
+     <template #en>
+      On this page
+     </template>
+     <template #ja>
+      項目
+     </template>
+    </Localization>
+   </Toc>
+  </template>
+  <template #default>
+   <Intro />
+   <b-div
+    id="content"
+   >
+    <slot />
+   </b-div>
+  </template>
+  <template #footer>
+   <ShareFooter />
+  </template>
+ </PageLayout>
 </template>
 
 <style>
