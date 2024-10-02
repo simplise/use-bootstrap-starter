dbe935e436bb6860ff348a465ffd04a51e664297
Kouji Sano <KoujiSano@users.noreply.github.com>
2024-10-02 18:10:46 +0900

    volta for local

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   compatibilityDate: "2024-07-03",
   modules: ["usebootstrap"],
   css: [
